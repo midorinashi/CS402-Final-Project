b53754742f27583a32704a24e3cb616efa17f5b1
midorinashi <traceylin@dn51vc9b.sunet>
2018-02-20 17:50:27 -0800

    Tuio working with reactivision

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import imageio
+imageio.plugins.ffmpeg.download()
+
+import matplotlib
+matplotlib.use('TkAgg')
 from moviepy.editor import *
 
-clip = VideoFileClip("lake.mp4")
-clip.ipython_display(width=280)
+import tuio
+tracking = tuio.Tracking()
+print "loaded profiles:", tracking.profiles.keys()
+print "list functions to access tracked objects:", tracking.get_helpers()
+try:
+    while 1:
+        tracking.update()
+        for obj in tracking.objects():
+            print obj
+except KeyboardInterrupt:
+    tracking.stop()
+
+
+
+clip = VideoFileClip("videoviewdemo.mp4")
+clip.preview()
