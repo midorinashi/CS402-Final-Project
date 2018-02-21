447ddfedabc2c26aa84294149e7e44984fbfb42d
midorinashi <traceylin@dn0a1f2b57.sunet>
2018-02-20 21:22:28 -0800

    added save functionality

--- main.py
+++ main.py
@@ -8,32 +8,44 @@ from moviepy.editor import *
 import tuio
 from pynput import keyboard
 
-def play():
+def concatenate():
+    clips = []
     try:
-    	#haxx to ensure the TUIO message is fresh - sometimes it's not??
-    	for i in range(20):
+        #haxx to ensure the TUIO message is fresh - sometimes it's not??
+        for i in range(20):
             tracking.update()
         print sum(1 for _ in tracking.objects())
         screensize = (720,460)
-        clips = []
         for obj in tracking.objects():
             print obj, obj.xpos, "hi"
             txtClip = TextClip(str(obj.id),color='white', font="Amiri-Bold",
                                kerning = 5, fontsize=100).set_pos('center').set_duration(2)
             clips.append(CompositeVideoClip([txtClip], size=screensize))
-        
-        if len(clips) > 0:
-            cvc = concatenate_videoclips(clips)
-            cvc.preview()
 
     except KeyboardInterrupt:
         tracking.stop()
+    return clips
+
+def play():
+    clips = concatenate()
+    if len(clips) > 0:
+        cvc = concatenate_videoclips(clips)
+        cvc.preview()
+
+
+def save():
+    clips = concatenate()
+    if len(clips) > 0:
+        cvc = concatenate_videoclips(clips)
+        cvc.write_videofile("video.mp4",fps=25,codec='mpeg4')
+
 
 def on_press(key):
-	# I can't get space and esc to work, so i'm using left and right shift instead for play and exit.
+    # I can't get space and esc to work, so i'm using left and right shift instead for play and exit.
     if key == keyboard.Key.shift_l:
-        print "hi"
         play()
+    if key == keyboard.Key.alt_l:
+        save()
 
 def on_release(key):
     print('{0} released'.format(
